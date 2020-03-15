22ab041a1118cee53a1dda8a2f025135bd38809a
Bekki Young <moonbladez@hotmail.com>
2020-03-15 10:30:56 +0100

    ammended function

--- js/question2.js
+++ js/question2.js
@@ -1,31 +1,32 @@
 const url = ("https://api.rawg.io/api/games")
 
+const createGames = () => {
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            handleJson(json)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 
-fetch(url)
-    .then((response) => {
-        return response.json()
-    })
-    .then((json) => {
-        handleJson(json)
-    })
-    .catch((error) => {
-        console.log(error)
-    });
+    const handleJson = (json) => {
+        const results = json.results
+        console.dir(results)
 
-const handleJson = (json) => {
-    const results = json.results
-    console.dir(results)
+        const resultsContainer = document.querySelector(".results")
+        let markUp = "";
 
-    const resultsContainer = document.querySelector(".results")
-    let markUp = "";
-
-    results.forEach((result) => {
-        markUp += `<div class="game">
+        results.forEach((result) => {
+            markUp += `<div class="game">
         <h2>${result.name}</h2>
         <img src="${result.background_image}" alt="${result.name}">
     </div>`
-        //could not see smaller images on JSON data
-    })
+            //could not see smaller images on JSON data
+        })
 
-    resultsContainer.innerHTML = markUp;
+        resultsContainer.innerHTML = markUp;
+    }
 }
